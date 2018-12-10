937834578bc226f89f321aa7b054158ca2efe550
Jaume <jaume.pladevall@est.fib.upc.edu>
2018-12-10 14:39:01 +0100

    Add files via upload

--- neatoCorners/cantonades.m
+++ neatoCorners/cantonades.m
@@ -1,7 +1,7 @@
 
 function xy = cantonades(points)
 xy = [];
-w = 6; %30;
+w = 16; %30 6;
 max_dist = 10000000; %5000;
 for i=1:1:length(points)-w
     dist = sqrt((points(i+w,1)-points(i,1))^2 + (points(i+w,2)-points(i,2))^2);
@@ -14,8 +14,8 @@ for i=1:1:length(points)-w
         pas3 = sqrt(ux^2 + uy^2) * sqrt(vx^2 + vy^2);
         pas1 = pas2/pas3;
         beta = acos(pas1)*(180/pi);
-        if (beta > 80 && beta < 100) || (beta > 260 && beta < 280)
-            xy = [xy; points(i+3,:)];
+        if (beta > 60 && beta < 120) || (beta > 240 && beta < 300)
+            xy = [xy; points(i+w/2,:)];
         end
     end
 end
--- neatoCorners/cluster.m
+++ neatoCorners/cluster.m
@@ -3,14 +3,14 @@ for i=1:1:length(xy)
     cont = 0;
     for j=1:1:length(xy)
         dist = sqrt((xy(i,1)-xy(j,1))^2 + (xy(i,2)-xy(j,2))^2);
-        if dist < 500 && i ~= j && dist ~= 0
+        if dist < 1000 && i ~= j && dist ~= 0
             cont = cont + 1;
         end
         %xy(i,:)
         %xy(j,:)
     end
     %xy(i,:), cont
-    if cont < 1 % 4
+    if cont < 3 %  3, 4
         xy(i,:) = nan;
     end
 end
